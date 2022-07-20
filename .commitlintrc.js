2c9a1e24e71f3c9e8b33c3cff0231612e44d9492
jesse <jesse152@163.com>
2022-07-21 07:27:26 +0800

    feat: generate changelog.md

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -5,6 +5,7 @@ module.exports = {
 			2,
 			"always",
 			[
+				"wip", // 开发中
 				"feat", // 新功能
 				"fix", // bug 修复
 				"docs", //文档变更
@@ -12,13 +13,8 @@ module.exports = {
 				"refactor", //重构
 				"perf", // 性能优化
 				"test", //新增或修订单元测试
-				"chore", //构建过程或辅助工具变更
-				"release",
-				"wip",
-				"build", // 构建执行
-				"ci", //CI 相关
 				"revert", // 回滚操作
-				"merge"
+				"chore" //构建过程或辅助工具变更
 			]
 		]
 	}
