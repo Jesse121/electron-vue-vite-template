32523401dec59b57af08482149d72383a9e69cdf
jesse <jesse152@163.com>
2022-07-21 21:01:03 +0800

    chore: vite set build minify:false

--- vite.config.ts
+++ vite.config.ts
@@ -1,50 +1,53 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
-import electron from "vite-plugin-electron";
 import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import Components from "unplugin-vue-components/vite";
+import { defineConfig } from "vite";
+import electron from "vite-plugin-electron";
 
 const resolvePath = (dir: string) => path.join(__dirname, dir);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".json"],
-    alias: {
-      "@": resolvePath("./src"),
-    },
-  },
-  plugins: [
-    vue(),
-    electron({
-      main: {
-        entry: "electron/main/index.ts",
-        vite: {
-          build: {
-            outDir: "dist/electron/main",
-          },
-        },
-      },
-      preload: {
-        input: {
-          // Must be use absolute path, this is the restrict of Rollup
-          index: resolvePath("electron/preload/index.ts"),
-        },
-        vite: {
-          build: {
-            sourcemap: "inline",
-            outDir: "dist/electron/preload",
-          },
-        },
-      },
-    }),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
+	resolve: {
+		extensions: [".ts", ".tsx", ".js", ".json"],
+		alias: {
+			"@": resolvePath("./src")
+		}
+	},
+	plugins: [
+		vue(),
+		electron({
+			main: {
+				entry: "electron/main/index.ts",
+				vite: {
+					build: {
+						outDir: "dist/electron/main"
+					}
+				}
+			},
+			preload: {
+				input: {
+					// Must be use absolute path, this is the restrict of Rollup
+					index: resolvePath("electron/preload/index.ts")
+				},
+				vite: {
+					build: {
+						sourcemap: "inline",
+						outDir: "dist/electron/preload"
+					}
+				}
+			}
+		}),
+		AutoImport({
+			resolvers: [ElementPlusResolver()]
+		}),
+		Components({
+			resolvers: [ElementPlusResolver()]
+		})
+	],
+	build: {
+		minify: false
+	}
 });
