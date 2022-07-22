f6da57a55ca9df1c9b05b52f21dd3e19b925a0b5
yangdong <yangdong@guotailimin.com>
2022-07-22 17:43:40 +0800

    feat: lint

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,12 +1,12 @@
 /// <reference types="vite/client" />
 
 declare module "*.vue" {
-  import type { DefineComponent } from "vue";
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
+	import type { DefineComponent } from "vue";
+	const component: DefineComponent<{}, {}, any>;
+	export default component;
 }
 declare const versions: {
-  node: () => string;
-  chrome: () => string;
-  electron: () => string;
+	node: () => string;
+	chrome: () => string;
+	electron: () => string;
 };
