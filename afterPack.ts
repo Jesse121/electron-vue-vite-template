d399b79b083aebe692064fa6ef25bbe66c387535
yangdong <yangdong@guotailimin.com>
2022-08-03 17:34:55 +0800

    feat: test

--- afterPack.ts
+++ afterPack.ts
@@ -12,18 +12,18 @@ const afterPack = context => {
 	var zip = new AdmZip();
 	zip.addLocalFolder(unpackedApp);
 	// 不常更改的包可排除
-	zip.deleteFile("node_modules/axios/");
-	zip.deleteFile("node_modules/adm-zip/");
-	zip.deleteFile("node_modules/element-plus/");
-	zip.deleteFile("node_modules/@element-plus/");
-	zip.deleteFile("node_modules/normalize.css/");
-	zip.deleteFile("node_modules/pinia/");
-	zip.deleteFile("node_modules/progress-stream/");
+	// zip.deleteFile("node_modules/axios/");
+	// zip.deleteFile("node_modules/adm-zip/");
+	// zip.deleteFile("node_modules/element-plus/");
+	// zip.deleteFile("node_modules/@element-plus/");
+	// zip.deleteFile("node_modules/normalize.css/");
+	// zip.deleteFile("node_modules/pinia/");
+	// zip.deleteFile("node_modules/progress-stream/");
 	zip.deleteFile("node_modules/sqlite3/");
-	zip.deleteFile("node_modules/sequelize/");
-	zip.deleteFile("node_modules/vue/");
-	zip.deleteFile("node_modules/@vue/");
-	zip.deleteFile("node_modules/vue-router/");
+	// zip.deleteFile("node_modules/sequelize/");
+	// zip.deleteFile("node_modules/vue/");
+	// zip.deleteFile("node_modules/@vue/");
+	// zip.deleteFile("node_modules/vue-router/");
 
 	zip.writeZip(path.join(context.outDir, "app.zip"));
 };
