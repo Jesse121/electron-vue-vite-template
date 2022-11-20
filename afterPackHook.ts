bca21280745620ce3b8165c5256c6ac507ca63b7
jesse <jesse152@163.com>
2022-11-20 09:39:20 +0800

    perf: move dependencies to devDependencies

--- afterPackHook.ts
+++ afterPackHook.ts
@@ -11,19 +11,6 @@ const afterPack = context => {
 	const unpackedApp = path.join(targetPath, "./app");
 	var zip = new AdmZip();
 	zip.addLocalFolder(unpackedApp);
-	// 不常更改的包可排除
-	zip.deleteFile("node_modules/axios/");
-	zip.deleteFile("node_modules/adm-zip/");
-	zip.deleteFile("node_modules/element-plus/");
-	zip.deleteFile("node_modules/@element-plus/");
-	zip.deleteFile("node_modules/normalize.css/");
-	zip.deleteFile("node_modules/pinia/");
-	zip.deleteFile("node_modules/progress-stream/");
-	zip.deleteFile("node_modules/sqlite3/");
-	zip.deleteFile("node_modules/sequelize/");
-	zip.deleteFile("node_modules/vue/");
-	zip.deleteFile("node_modules/@vue/");
-	zip.deleteFile("node_modules/vue-router/");
 
 	zip.writeZip(path.join(context.outDir, "app.zip"));
 };
