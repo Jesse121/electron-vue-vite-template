61e7e9e51d703c275b185d4406623174e8cc95a8
jesse <jesse152@163.com>
2022-11-21 22:06:33 +0800

    feat: handleDatabase

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -29,7 +29,7 @@ class ElectronApp implements IElectronApp {
 			this.initMainWin();
 			this.initOtherWin();
 			this.initListenFullUpdate();
-			// handleDatabase();
+			handleDatabase();
 
 			this.loadModules();
 			loadVueDevtools();
